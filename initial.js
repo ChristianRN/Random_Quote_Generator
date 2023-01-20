5985592c0687ef288944cc56d5fc3db747d401a8
ChristianRN <crnegrete14@gmail.com>
2023-01-19 16:17:59 -0800

    Quotes added w/ random function

---
+++ initial.js
@@ -0,0 +1,14 @@
+//----14----//
+const Quotes = ["If God is all you have, you have all you need. ~John 14:8", "Your focus determines you reality. ~Qui Gon Jinn",
+"Scince is the faith that the future will behave like the past.", "How we walk with the broken speaks louder than how we sit with the great. ~B. Bennot",
+"Life moves pretty fast. You don't stop and look around once in a while. You could miss it. ~Ferris Bueller",
+"I have spent so much time with myself, yet I still don't know who I really am.", "The only way to discover the limits of the possible is to go beyond them into the impossible. ~Arthur C. Clarke",
+"Now I have to remember you for longer than I've known you.", "God will put you back togther, right in front of the people who broke you.", 
+"There is no passion to be found...playing small, and settling for a life that's less, than the one you're capable of living. ~Nelson Mandela",
+"Better to have a short life...that is full of what you like doing, than a long life spent in a miserable way.",
+"We are all homesick for a place we aren't even sure exists.", 
+"If you always do what you always did you'll always get what you always got. You cannot change you life unless you change something.",
+"Nothing changes if NOTHING changes."]
+
+const quote = Quotes[Math.floor(Math.random()* Quotes.length)];
+console.log(quote);
